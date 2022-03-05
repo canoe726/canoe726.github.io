b4d4c262c654e0b7f343f2bd06c98cfaa87ece9e
canoe726 <canoe918@gmail.com>
2022-03-06 02:56:09 +0900

    Set max width in post container

--- frontend/pages/post/[category]/[slug].tsx
+++ frontend/pages/post/[category]/[slug].tsx
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import type { NextPage } from 'next'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Container, Flex, Text } from '@chakra-ui/react'
 import { FrontMatter, PostsData, postsDataState } from '../../../stores/posts'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import markdownToHtml from '../../../utils/markdownToHtml'
@@ -76,37 +76,40 @@ const Post: NextPage<PostProps> = ({
         <title>{frontmatter.title}</title>
         <meta name="description" content={`${frontmatter.title}-${frontmatter.summary}`}></meta>
       </Head>
-      <Box position='relative' padding={['35vh 1.5em 4em 1.5em', '35vh 2.5em 4em 2.5em', '35vh 4em 4em 4em']}>
-        <Box
-          position='absolute'
-          overflow='hidden'
-          top='64px'
-          left={['1.5em', '2.5em', '4em']}
-          right={['1.5em', '2.5em', '4em']}
-          height='35vh'
-        >
-          <Box ref={imageZoomBoxRef} width='100%' height='100%' position='relative'>
-            <Image
-              layout='fill'
-              objectFit='cover'
-              alt={`${frontmatter.category}-${slug}`}
-              src={`/post/${frontmatter.category}/${slug}/${frontmatter.coverImage}`}
-              loader={imageLoader}
-              unoptimized={true}
-              priority={true}
-            ></Image>
+      <Container padding='0' maxWidth='1320px'>
+        <Box position='relative' padding={['35vh 1.5em 4em 1.5em', '35vh 2.5em 4em 2.5em', '35vh 4em 4em 4em']}>
+          <Box
+            position='absolute'
+            overflow='hidden'
+            top='64px'
+            left={['1.5em', '2.5em', '4em']}
+            right={['1.5em', '2.5em', '4em']}
+            height='35vh'
+            maxHeight='420px'
+          >
+            <Box ref={imageZoomBoxRef} width='100%' height='100%' position='relative'>
+              <Image
+                layout='fill'
+                objectFit='cover'
+                alt={`${frontmatter.category}-${slug}`}
+                src={`/post/${frontmatter.category}/${slug}/${frontmatter.coverImage}`}
+                loader={imageLoader}
+                unoptimized={true}
+                priority={true}
+              ></Image>
+            </Box>
           </Box>
+          <Box position='absolute' top='64px' left={['1.5em', '2.5em', '4em']} right={['1.5em', '2.5em', '4em']} height='35vh' maxHeight='420px' backgroundColor='rgba(0, 0, 0, 0.4)'>
+            <Text fontWeight='normal' position='absolute' width='100%' padding={['0 0.5em 0 0.5em', '0 0.5em 0 0.5em', '0 1em 0 1em']} top='40%' left='50%' transform='translate(-50%, -40%)' textAlign='center' lineHeight={[1.2, 1.2, 1.5]} fontSize={['3xl', '4xl', '5xl']} color='white'>{frontmatter.title}</Text>
+            {frontmatter.shortcut && (
+              <Text fontWeight='light' position='absolute' top='70%' left='50%' padding={['0 0.5em 0 0.5em', '0 0.5em 0 0.5em', '0 1em 0 1em']} transform='translate(-50%, -70%)' fontSize={['md', 'xl', '2xl']} textAlign='center' color='gray.100'>{frontmatter.shortcut}</Text>
+            )}
+          </Box>
+          <Flex margin='64px 0 0 0' width='100%' justifyContent='center' alignItems='center'>
+            <article className='post-body' style={{ width: '100%', maxWidth: '1320px' }} dangerouslySetInnerHTML={{ __html: htmlContent }}></article>
+          </Flex>
         </Box>
-        <Box position='absolute' top='64px' left={['1.5em', '2.5em', '4em']} right={['1.5em', '2.5em', '4em']} height='35vh' backgroundColor='rgba(0, 0, 0, 0.4)'>
-          <Text fontWeight='normal' position='absolute' width='100%' padding={['0 0.5em 0 0.5em', '0 0.5em 0 0.5em', '0 1em 0 1em']} top='40%' left='50%' transform='translate(-50%, -40%)' textAlign='center' lineHeight={[1.2, 1.2, 1.5]} fontSize={['3xl', '4xl', '5xl']} color='white'>{frontmatter.title}</Text>
-          {frontmatter.shortcut && (
-            <Text fontWeight='light' position='absolute' top='70%' left='50%' padding={['0 0.5em 0 0.5em', '0 0.5em 0 0.5em', '0 1em 0 1em']} transform='translate(-50%, -70%)' fontSize={['md', 'xl', '2xl']} textAlign='center' color='gray.100'>{frontmatter.shortcut}</Text>
-          )}
-        </Box>
-        <Flex margin='64px 0 0 0' width='100%' justifyContent='center' alignItems='center'>
-          <article className='post-body' style={{ width: '100%', maxWidth: '1320px' }} dangerouslySetInnerHTML={{ __html: htmlContent }}></article>
-        </Flex>
-      </Box>
+      </Container>
     </>
   )
 }
