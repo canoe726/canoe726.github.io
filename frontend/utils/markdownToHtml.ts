0ac05e2141f0335f259c96c3049498f8826f6e6c
canoe726 <canoe918@gmail.com>
2022-03-06 02:08:19 +0900

    Add highlight.js, markdownit library

--- frontend/utils/markdownToHtml.ts
+++ frontend/utils/markdownToHtml.ts
@@ -1,13 +1,22 @@
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-// import remark from 'remark'
-import remarkHtml from 'remark-html'
+const hljs = require('highlight.js')
+const markDownIt = require('markdown-it')({
+  html: true,
+  xhtmlOut: true,
+  breaks: true,
+  langPrefix: 'language-',
+  linkify: false,
+  highlight: function (str: string, lang: string) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+          '</code></pre>'
+      } catch (__) {}
+    }
+    return '<pre class="hljs"><code>' + markDownIt.utils.escapeHtml(str) + '</code></pre>'
+  }
+})
 
 export default async function markdownToHtml (markdown: any) {
-  // const result = await remark().use(html).process(markdown)
-  const file = await unified()
-    .use(remarkParse)
-    .use(remarkHtml)
-    .process(markdown)
-  return file.toString()
+  return markDownIt.render(markdown)
 }
