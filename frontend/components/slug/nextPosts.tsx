48e8cfbadf31ebf163a698835c8ff3e4c8807b6b
canoe726 <canoe918@gmail.com>
2022-03-06 15:10:03 +0900

    Add post bottom buttons layout

--- frontend/components/slug/nextPosts.tsx
+++ frontend/components/slug/nextPosts.tsx
@@ -1,20 +1,50 @@
 import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { NextPage } from 'next'
+import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { postsDataState } from '../../stores/posts'
 
 const NextPosts = () => {
   const postsData = useRecoilValue(postsDataState)
+  // const [filtered, setFiltered] = useState<>()
 
   return (
-    <Flex flexDirection='column' padding={['0 1.5em 4em 1.5em', '0 2.5em 4em 2.5em', '0 4em 4em 4em']}>
-      <Box>
-        <Button>{'< Prev'}</Button>
-        <Button>{'Next >'}</Button>
+    <Flex justifyContent='center' alignItems='center' flexDirection='column' padding={['0 1.5em 4em 1.5em', '0 2.5em 4em 2.5em', '0 4em 4em 4em']}>
+      <Box display='flex'>
+        <Button margin='0 0.5em 0 0.5em'>{'< Prev'}</Button>
+        <Button margin='0 0.5em 0 0.5em'>{'Next >'}</Button>
       </Box>
-      <Box>
-        <Text>Next Posts</Text>
+      <Box margin='1em 0 0 0'>
+        <Text fontWeight='normal'>Related Posts</Text>
+        {postsData.map((postData, idx) => {
+          return (
+            <Text key={idx}>
+              {postData.category}
+            </Text>
+          )
+        })}
       </Box>
+      <NextPostList data={postsData}></NextPostList>
     </Flex>
+  )
+}
+
+interface NextPostListProps {
+  data: any;
+}
+
+const NextPostList: NextPage<NextPostListProps> = ({ data }) => {
+  return (
+    <Box margin='1em 0 0 0'>
+      <Text fontWeight='normal'>Random Posts</Text>
+      {data.map((postData, idx) => {
+        return (
+          <Text key={idx}>
+            {postData.category}
+          </Text>
+        )
+      })}
+    </Box>
   )
 }
 
